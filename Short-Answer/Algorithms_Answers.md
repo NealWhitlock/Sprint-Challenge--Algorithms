443760362f273a55c109d44f0f941a15d5657e1c
NealWhitlock <NealWhitlock@users.noreply.github.com>
2020-06-19 11:42:46 -0600

    Done and tested. And I'm putting this comment in here for posterity. That robot sort was terrible!

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,14 +2,18 @@
 
 ## Exercise I
 
-a)
+a) If we were simply incrementing or decrementing from a the runtime would be O(n). But what I gather from looking into runtimes is that with the multiplication taking place the runtime ends up being O(logn). I'm not entirely sure why, though. 
 
 
-b)
+b) With nested loops the runtime is O(n^2) because for each iteration of the outside loop, with runtime O(n), the inside loop also has a runtime of O(n).
 
 
-c)
+c) Since this recursion is just being called once in the return it should have a runtime of O(n). After all, we're just adding the amount of bunnies until there aren't anymore to add.
 
 ## Exercise II
 
+Runtime complexity: O(logn)
 
+Given a height of n, I'd go to floor n/2 and drop an egg. If it broke I'd then go to floor (n/2)/2 = n/4 and drop an egg. If the first egg did not break then I would move up to floor 3n/4 and drop an egg. 
+
+What is happening is that I am taking the whole interval of unknown and splitting it in half. Drop an egg from that floor. If the egg breaks then we're too high so the top of the next interval to search becomes the floor I just dropped an egg from and repeat the process. If the egg did not break then we can go higher and the floor I just dropped an egg from becomes the bottom of the next interval to be split in half. Each time an egg breaks that floor becomes the top. Each time an egg doesn't break that floor becomes the bottom. This repeats for logn times until we have found two adjacent floors where the bottom one is low enough that the egg doesn't break but the one above it the egg does break.
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,8 +96,68 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+
+
+        # What's the light for? How will we know we're done?
+        # If we still have swapping to do (or at least checking to do), turn the light on?
+        # If the light is off, move right?
+
+        # Pick up the first item and turn on the light
+        self.swap_item()
+        self.set_light_on()
+        # 
+        # While the light is on
+        while self.light_is_on():
+            # While robot can still move right
+            while self.can_move_right():
+                
+                                    # # Compare items
+                                    # # If held item is greater than not held item  # Getting stuck here
+                                    # if self.compare_item() == 1:
+                                    #     # Continue
+                                    #     continue
+                                    # # Else
+                                    # else:
+                                    #     # Swap items
+                                    #     self.swap_item()
+                
+                # If the item the robot is holding is smaller
+                if self.compare_item() == -1:
+                    # Swap them  
+                    self.swap_item()
+                    # Move right
+                    self.move_right()
+                else:
+                    # Move right
+                    self.move_right()
+                    # # Just continue
+                    # continue
+
+
+
+            # If robot gets to end of the list and has no item in hand, then we're done
+            if self.can_move_right() == False and self.compare_item() == None:    # <- AHHHHHHHHHHH!!!!!!! I had can_move_left for SO long!!!!
+                self.swap_item()
+                # Turn off the light
+                self.set_light_off()
+            
+                            # What if the first item is the lowest value?
+                            # The robot needs to return to the first position and drop the item
+            # While robot can move left
+            else:
+                while self.can_move_left():
+                    # Move left
+                    self.move_left()
+                    # If it's found the empty spot
+                    if self.compare_item() == None:
+                        # Drop item
+                        self.swap_item()
+                        # Move right
+                        self.move_right()
+                        # Pick up item
+                        self.swap_item()
+
+    
 
 
 if __name__ == "__main__":
@@ -105,6 +165,7 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    #l = [3,2,1]
 
     robot = SortingRobot(l)
 
